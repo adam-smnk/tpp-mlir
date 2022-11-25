397fbaa23ac7645516c9af47ae61f375a87fc39a
Adam Siemieniuk <adam.siemieniuk@intel.com>
2022-11-25 11:27:49 +0100

    Init perf dialect

---
+++ lib/TPP/Dialect/Perf/PerfOps.cpp
@@ -0,0 +1,17 @@
+//===- PerfOps.cpp - Perf dialect ops ---------------------------*- C++ -*-===//
+//
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "TPP/Dialect/Perf/PerfOps.h"
+#include "TPP/Dialect/Perf/PerfDialect.h"
+#include "mlir/IR/OpImplementation.h"
+
+#define GET_OP_CLASSES
+#include "TPP/Dialect/Perf/PerfOps.cpp.inc"
+
+using namespace mlir;
+using namespace mlir::perf;
--- tpp-opt/tpp-opt.cpp
+++ tpp-opt/tpp-opt.cpp
@@ -23,8 +23,9 @@
 #include "TPP/Dialect/Check/CheckDialect.h"
 #include "TPP/Dialect/LinalgX/BufferizableOpInterfaceImpl.h"
 #include "TPP/Dialect/LinalgX/LinalgXDialect.h"
-#include "TPP/Dialect/Transform/LinalgXTransformOps.h"
+#include "TPP/Dialect/Perf/PerfDialect.h"
 #include "TPP/Dialect/Tpp/TppDialect.h"
+#include "TPP/Dialect/Transform/LinalgXTransformOps.h"
 #include "TPP/Dialect/VNNI/BufferizableOpInterfaceImpl.h"
 #include "TPP/Dialect/VNNI/VNNIDialect.h"
 #include "TPP/Dialect/VNNI/TransformOps/VNNITransformOps.h"
@@ -41,6 +42,7 @@ int main(int argc, char **argv) {
   registry.insert<mlir::linalgx::LinalgXDialect>();
   registry.insert<mlir::check::CheckDialect>();
   registry.insert<mlir::vnni::VNNIDialect>();
+  registry.insert<mlir::perf::PerfDialect>();
   mlir::linalgx::registerTransformDialectExtension(registry);
   mlir::linalgx::registerBufferizableOpInterfaceExternalModels(registry);
   mlir::check::registerBufferizableOpInterfaceExternalModels(registry);

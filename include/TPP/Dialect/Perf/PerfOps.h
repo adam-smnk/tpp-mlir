c862c2ad574fcb91408b0e0d9a1a4d249667b1a3
Adam Siemieniuk <adam.siemieniuk@intel.com>
2023-04-12 16:52:11 +0200

    PoC perf.time
    Call-like perf op to measure only single call.
    Basic op infrastructure is ready.
    Roadblock hit on bufferization - it looks like the whole
    func_ext::CallOpInterface would have to be reimplemented.
      -> alternative approach by marking ops

--- include/TPP/Dialect/Perf/PerfOps.h
+++ include/TPP/Dialect/Perf/PerfOps.h
@@ -10,10 +10,13 @@
 #define TPP_DIALECT_PERF_PERFOPS_H
 
 #include "TPP/Dialect/Perf/PerfTypes.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/IR/BuiltinTypes.h"
 #include "mlir/IR/Dialect.h"
 #include "mlir/IR/OpDefinition.h"
 #include "mlir/IR/OpImplementation.h"
+#include "mlir/IR/SymbolTable.h"
+#include "mlir/Interfaces/CallInterfaces.h"
 #include "mlir/Interfaces/ControlFlowInterfaces.h"
 #include "mlir/Interfaces/InferTypeOpInterface.h"
 #include "mlir/Interfaces/SideEffectInterfaces.h"

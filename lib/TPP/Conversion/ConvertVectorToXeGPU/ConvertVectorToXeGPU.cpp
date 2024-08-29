27c5d673b497350d3e47febfc75811c267a23612
Adam Siemieniuk <adam.siemieniuk@intel.com>
2024-08-29 13:57:48 +0200

    Lower transfer read

--- lib/TPP/Conversion/ConvertVectorToXeGPU/ConvertVectorToXeGPU.cpp
+++ lib/TPP/Conversion/ConvertVectorToXeGPU/ConvertVectorToXeGPU.cpp
@@ -9,11 +9,15 @@
 #include "TPP/Passes.h"
 #include "TPP/Transforms/Utils/ValueUtils.h"
 
+#include "mlir/Dialect/Arith/IR/Arith.h"
 #include "mlir/Dialect/LLVMIR/LLVMDialect.h"
 #include "mlir/Dialect/MemRef/IR/MemRef.h"
 #include "mlir/Dialect/Vector/IR/VectorOps.h"
 #include "mlir/Dialect/XeGPU/IR/XeGPU.h"
 #include "mlir/Transforms/GreedyPatternRewriteDriver.h"
+
+#include <algorithm>
+#include <optional>
 
 using namespace mlir;
 
@@ -31,6 +35,8 @@ struct TransferReadLowering : public OpRewritePattern<vector::TransferReadOp> {
 
   LogicalResult matchAndRewrite(vector::TransferReadOp readOp,
                                 PatternRewriter &rewriter) const override {
+    Location loc = readOp.getLoc();
+
     if (readOp.hasOutOfBoundsDim())
       return rewriter.notifyMatchFailure(
           readOp, "Expects all dimensions to be in-bounds");
@@ -73,9 +79,66 @@ struct TransferReadLowering : public OpRewritePattern<vector::TransferReadOp> {
       return rewriter.notifyMatchFailure(
           readOp, "Unsupported data type for tranposition");
 
-    // auto descType = xegpu::TensorDescType::get(vecTy.getShape(),
-    // elementType); auto ndDesc = rewriter.create<xegpu::CreateNdDescOp>(
-    //     loc, descType, readOp.getSource(), offsets);
+    auto descType = xegpu::TensorDescType::get(vecTy.getShape(), elementType);
+    TypedValue<ShapedType> src = readOp.getSource();
+    Operation::operand_range offsets = readOp.getIndices();
+    xegpu::CreateNdDescOp ndDesc;
+    if (srcTy.hasStaticShape()) {
+      ndDesc = rewriter.create<xegpu::CreateNdDescOp>(
+          loc, descType, dyn_cast<TypedValue<MemRefType>>(src),
+          getAsOpFoldResult(offsets));
+    } else {
+      SmallVector<Value> sourceDims;
+      unsigned srcRank = srcTy.getRank();
+      for (unsigned i = 0; i < srcRank; ++i)
+        sourceDims.push_back(rewriter.create<memref::DimOp>(loc, src, i));
+
+      SmallVector<int64_t> constOffsets;
+      SmallVector<Value> dynOffsets;
+      for (auto offset : offsets) {
+        std::optional<int64_t> staticVal = getConstantIntValue(offset);
+        if (!staticVal)
+          dynOffsets.push_back(offset);
+        constOffsets.push_back(staticVal ? *staticVal : ShapedType::kDynamic);
+      }
+
+      SmallVector<Value> dynShapes;
+      for (auto [idx, shape] : llvm::enumerate(srcTy.getShape())) {
+        if (shape == ShapedType::kDynamic)
+          dynShapes.push_back(sourceDims[idx]);
+      }
+
+      // Compute strides in reverse order.
+      SmallVector<Value> dynStrides;
+      Value accStride = rewriter.create<arith::ConstantIndexOp>(loc, 1);
+      // Last stride is guaranteed to be static and unit.
+      for (int i = static_cast<int>(strides.size()) - 2; i >= 0; --i) {
+        accStride =
+            rewriter.create<arith::MulIOp>(loc, accStride, sourceDims[i + 1]);
+        if (strides[i] == ShapedType::kDynamic)
+          dynStrides.push_back(accStride);
+      }
+      std::reverse(dynStrides.begin(), dynStrides.end());
+
+      ndDesc = rewriter.create<xegpu::CreateNdDescOp>(
+          loc, descType, dyn_cast<TypedValue<MemRefType>>(src), dynOffsets,
+          dynShapes, dynStrides,
+          DenseI64ArrayAttr::get(rewriter.getContext(), constOffsets),
+          DenseI64ArrayAttr::get(rewriter.getContext(), srcTy.getShape()),
+          DenseI64ArrayAttr::get(rewriter.getContext(), strides));
+    }
+
+    auto transposeAttr = !isTransposeLoad
+                             ? nullptr
+                             : DenseI64ArrayAttr::get(rewriter.getContext(),
+                                                      ArrayRef<int64_t>{1, 0});
+    // By default, no specific caching policy is assigned.
+    xegpu::CachePolicyAttr hint = nullptr;
+    auto loadOp = rewriter.create<xegpu::LoadNdOp>(
+        loc, vecTy, ndDesc, /*packed=*/nullptr, transposeAttr,
+        /*l1_hint=*/hint,
+        /*l2_hint=*/hint, /*l3_hint=*/hint);
+    rewriter.replaceOp(readOp, loadOp);
 
     return success();
   }
